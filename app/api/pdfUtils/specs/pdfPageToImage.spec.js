c79fe78407a7bfbc8960f7e9478b6defed02bb99
RafaPolit <rafaelpolit@gmail.com>
2018-04-17 12:27:39 -0500

    Changed snapshot image testing.

--- app/api/pdfUtils/specs/pdfPageToImage.spec.js
+++ app/api/pdfUtils/specs/pdfPageToImage.spec.js
@@ -23,30 +23,33 @@ describe('pdfPageToImage PDF utility', () => {
   function deleteCreatedFiles(done) {
     safeDeleteFile(`${__dirname}/../fixtures/batmanFullPage1.png`)
     .then(() => safeDeleteFile(`${__dirname}/../fixtures/jokerThumbPage2.jpg`))
-    .then(() => safeDeleteFile(`${__dirname}/../fixtures/jokerFullPage4.jpg`))
+    .then(() => safeDeleteFile(`${__dirname}/../fixtures/jokerFullPagePage1.png`))
     .then(done)
     .catch(done.fail);
   }
 
-  function expectIdenticalFiles(file1URL, done) {
-    //const file1 = fs.readFileSync(`${__dirname}${file1URL}`);
-    Jimp.read(`${__dirname}${file1URL}`).then((image) => {
-      image.getBuffer(Jimp.MIME_PNG, (err, result) => {
-        if (err) {
-          throw err;
-        }
-        expect(result).toMatchImageSnapshot();
-        done();
-      });
-    })
-    .catch(done.fail);
+  function expectIdenticalFiles(file1URL, reBuffer, done) {
+    if (!reBuffer) {
+      const file1 = fs.readFileSync(`${__dirname}${file1URL}`);
+      expect(file1).toMatchImageSnapshot();
+      done();
+    } else {
+      Jimp.read(`${__dirname}${file1URL}`).then((image) => {
+        image.getBuffer(Jimp.MIME_PNG, (err, result) => {
+          if (err) { throw err; }
+          expect(result).toMatchImageSnapshot();
+          done();
+        });
+      })
+      .catch(done.fail);
+    }
   }
 
-  function expectPDFResults([originalPDF, output, options, done]) {
+  function expectPDFResults([originalPDF, output, options, reBuffer, done]) {
     pdfPageToImage(`${__dirname}/../fixtures/${originalPDF}`, `${__dirname}/../fixtures/${output}`, options)
     .then((res) => {
       expect(res).toContain('Finished converting page to');
-      expectIdenticalFiles(`/../fixtures/${output}`, done);
+      expectIdenticalFiles(`/../fixtures/${output}`, reBuffer, done);
     })
     .catch(done.fail);
   }
@@ -57,19 +60,18 @@ describe('pdfPageToImage PDF utility', () => {
 
   describe('Basic Usage', () => {
     it('should save page 1 to specified output with default parameters (PNG, full size)', (done) => {
-      expectPDFResults(['batman.pdf', 'batmanFullPage1.png', { scale: 0.1 }, done]);
+      expectPDFResults(['batman.pdf', 'batmanFullPage1.png', undefined, false, done]);
     });
   });
 
   describe('Advanced Options', () => {
-    it('should save allow saving a different page, scale and format', (done) => {
-      const options = { format: 'jpg', scale: 0.1, page: 2 };
-      expectPDFResults(['joker.pdf', 'jokerThumbPage2.jpg', options, done]);
+    it('should allow saving a different page, scale and format', (done) => {
+      const options = { format: 'jpg', scale: 0.2, page: 2 };
+      expectPDFResults(['joker.pdf', 'jokerThumbPage2.jpg', options, true, done]);
     });
 
-    it('should save allow overriding certain params only', (done) => {
-      const options = { format: 'jpg', scale: 0.1, page: 4 };
-      expectPDFResults(['joker.pdf', 'jokerFullPage4.jpg', options, done]);
+    it('should allow overriding certain params only', (done) => {
+      expectPDFResults(['joker.pdf', 'jokerFullPagePage1.png', { scale: 0.3 }, false, done]);
     });
   });
 
